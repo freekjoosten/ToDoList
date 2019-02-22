b49e399c0b6305585b53866d56416038ef0773cb
freekjoosten <45119063+freekjoosten@users.noreply.github.com>
2019-02-22 12:43:58 +0100

    zo dan
    Lukt het? NEE!

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ const button = document.querySelector(".button");
 const listItem = document.querySelector(".errandlist");
 
 
-// Luisteren voor klik
-button.addEventListener('click', function (event) {
-    console.log(textInput.value);
+
+// Luisteren voor klik Voeg Toe
+button.addEventListener('click', function () {
+    
     if (textInput.value === "") {
     window.alert('Een leeg klusje is geen klusje! Vul wat in!');
     } else {
         listItem.innerHTML += `<li class="errand"><div class="left">
-        <span class="erranddone">&check;</span>${textInput.value}</div><div class="right erranddelete">
+        <span class="erranddone">&check;</span><p class="strike">${textInput.value}</p></div><div class="right erranddelete">
         X</li>`
     }
 });
 
 
+// Hieronder ben ik aan het rotzooien!
+const errandDone = document.querySelector(".erranddone");
+const strikeThrough = document.querySelector(".strike");
+const errand = document.querySelector(".errand");
+
+errand.addEventListener('click', function (event) {
+    console.log(event.target.classList);
+    if (event.target.classList.contains('erranddelete')) {
+        console.log("is dit 1");
+        return;
+    } else {
+        console.log("toch 2");
+        event.currentTarget.classList.toggle('checked');
+    }
+
+});
 
 
-// Klik op knop is toevoegen
 
 
 // Vink is afvinken
